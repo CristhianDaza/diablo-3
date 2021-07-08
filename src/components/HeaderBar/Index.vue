10aa9cf03dd4e82603d15a8bc7027c0c7087f71f
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 14:58:44 -0500

    Agregando componente header

---
+++ src/components/HeaderBar/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header-bar">
+    <div class="navigation-bar">
+      <b-nabvar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand :to="{ name: 'Home' }">
+          <img src="@/assets/img/diablo-iii.svg" alt="Diablo 3" width="30">
+          <span class="font-diablo ml-2">D3PF</span>
+        </b-navbar-brand>
+      </b-nabvar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBar'
+}
+</script>
