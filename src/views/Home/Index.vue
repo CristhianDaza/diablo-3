2774673b531aec0c320bbf1dae3ce0417b2246f0
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 16:20:27 -0500

    Creando Formulario de busqueda y agregandolo al home

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <HomeTitle />
+    <HomeForm />
   </div>
 </template>
 
 <script>
 import HomeTitle from './HomeTitle.vue'
+import HomeForm from './HomeForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    HomeTitle
+    HomeTitle,
+    HomeForm
   }
 }
 </script>
