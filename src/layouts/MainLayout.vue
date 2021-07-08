bd07d47f648f6c79e59f5bd0d968711af2a8b6a5
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 15:24:56 -0500

    Agregando compoentes HeaderBar y FooterBar al Layout principal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <HeaderBar />
     <div class="container">
       <router-view/>
     </div>
+    <FooterBar />
   </div>
 </template>
 
 <script>
+import HeaderBar from '@/components/HeaderBar/Index.vue'
+import FooterBar from '@/components/FooterBar/Index.vue'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: {
+    HeaderBar,
+    FooterBar
+  }
 }
 </script>
