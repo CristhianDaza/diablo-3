65b2176e2954fd826c8ecbac1f80fa1a4b449fa0
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 15:26:58 -0500

    Agregando FontAweome

--- src/main.js
+++ src/main.js
@@ -4,11 +4,31 @@ import Vue from 'vue'
 import './plugins/axios'
 import './plugins/bootstrap-vue'
 import './assets/css/main.styl'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
