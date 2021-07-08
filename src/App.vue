08b76e9928d6a29924db141ed741f579ed783cd7
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 13:05:59 -0500

    Agregando estilos principales

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,12 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+body
+  padding 60px 0
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing grayscale
+  color #fff
+  background-color #15202b
+</style>
