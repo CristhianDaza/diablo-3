f639e706ec1442f333f28cd51f539896f0ad331b
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 15:16:03 -0500

    Agregando componentes de footer

---
+++ src/components/FooterBar/FootLinks.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-nav small align="start">
+    <b-nav-item :to="{ name: 'Home'}">
+      Home
+    </b-nav-item>
+    <b-nav-item href="https://github.com/CristhianDaza/diablo-3" target="_blank">
+      GitHub
+    </b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {
+  name: 'FootLinks'
+}
+</script>
---
+++ src/components/FooterBar/MadeByFoot.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-muted m-0 pt-1 pl3 text-left text-sm-right">
+    Made by
+    <a href="https://github.com/CristhianDaza">Cristhian Daza</a>
+    With 💙
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'MadeByFoot'
+}
+</script>
