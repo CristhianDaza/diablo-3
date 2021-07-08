e57bb10d714649afa6c32a42251732511a084ed6
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 16:00:29 -0500

    Agregando componente de titulo

---
+++ src/views/Home/HomeTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-title text-center">
+    <h1 class="my-5 font-diablo">
+      Diablo 3 Profile Finder
+    </h1>
+    <p class="lead text-muted">Enter your
+      <em>
+        <a
+          href="https://eu.battle.net/support/es/article/75767"
+          target="_blank"
+          title="Format: YourProfile#1234"
+        >
+          and select your region to see your profile
+        </a>
+        <hr class="mt-5">
+      </em>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeTitle'
+}
+</script>
