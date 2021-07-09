35187efbfedf170aaff45db4847783fedbd5f36e
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 22:07:48 -0500

    Creando componente TopHero

---
+++ src/views/Profile/MainBlock/TopHeroes/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="top-heroes">
+    <h1>Top Heroes</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TopHeroes',
+  props: {
+    heroes: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
---
+++ src/views/Profile/MainBlock/TopHeroes/TopHero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>TopHero</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopHero',
+  props: {
+    hero: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
