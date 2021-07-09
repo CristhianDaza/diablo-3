8e94d4740a808ac8f27b602bbe5a419cae0fb3c9
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 20:58:36 -0500

    Agregando stilos basicos

--- src/views/Profile/MainBlock/Index.vue
+++ src/views/Profile/MainBlock/Index.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Main Block</h1>
+  <div class="grid-container">
+    <div class="grid-item item-left">
+      <h1>Izquierda</h1>
+    </div>
+    <div class="grid-item item-right">
+      <h1>Derecha</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +20,25 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+  .grid-container
+    display grid
+    grid-template-columns 1fr
+
+    .grid-item
+      &.item-left
+        grid-column span 1
+      &.item-right
+        grid-column span 1
+
+  @media (min-width: 992px)
+    .grid-container
+      display grid
+      grid-template-columns repeat(6, 1fr)
+      .grid-item
+        &.item-left
+          grid-column span 4
+        &.item-right
+          grid-column span 2
+</style>
