73eaedc4f6ea6ffc35dced5303ed06030679a847
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-08 19:44:35 -0500

    Agregando rutas al router

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,20 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home/Index.vue')
-  }
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+  { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+  { path: '/error', name: 'Error' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
+
+const routes = routerOptions.map(r => {
+  return {
+    ...r,
+    component: () => import(`@/views/${r.name}/Index.vue`)
+  }
+})
 
 const router = new VueRouter({
   routes
